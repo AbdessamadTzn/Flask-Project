cdee3ed64d60a0fc49f8e108244fc599dbf36245
AbdessamadTzn <abdessamadtouzani07@gmail.com>
2024-01-06 18:03:04 +0100

    Update: start over app

--- app.py
+++ app.py
@@ -1,44 +1,11 @@
-import os
-from flask import Flask, render_template, redirect, url_for, request, flash, jsonify
-from flask_sqlalchemy import SQLAlchemy
-import re
-
-
-from extensions import db
-DB_NAME = "abdessamad.db"
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'DJODNCWOICNWOIEACJOIEWJ'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-
-from views.views import views
-from auth.teachersRoutes import authTeachers
-
-
-app.register_blueprint(views, url_prefix='/')
-app.register_blueprint(authTeachers, url_prefix='/')
-
-
-
-# with app.app_context():
-#     teacher_db.create_all()
-
-
-
-
-
-
-
-
-
-#Use it for checking perfermance of signing up...
-
+@app.route('/')
+def index():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run()
