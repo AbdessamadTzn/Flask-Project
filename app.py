02d817ea0290edc261c051afd131bf5b7bca41e9
AbdessamadTzn <abdessamadtouzani07@gmail.com>
2024-01-10 23:50:08 +0100

    Fixing models bugs

--- app.py
+++ app.py
@@ -2,15 +2,23 @@ import os
 from flask import Flask, render_template, jsonify
 import psycopg2
 from dotenv import load_dotenv
+from extensions import db
+from models import Teacher, Student
 
 load_dotenv()
 
 app = Flask(__name__)
+
 try:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 except Exception as e:
     print(f'Error loading Data Base: {e}')
 
+db.init_app(app)
+
+
+# with app.app_context():
+#     db.create_all()
 
 
 @app.route('/')
