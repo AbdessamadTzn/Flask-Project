b5c38f59d88c94bb4d08b0de3b5e29a4b3f5c627
AbdessamadTzn <abdessamadtouzani07@gmail.com>
2024-01-22 23:28:23 +0100

    [feat] login for students

--- auth/Students.py
+++ auth/Students.py
@@ -1,9 +1,30 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 import re
 from passlib.hash import pbkdf2_sha256
+from models import Teacher
+from extensions import db
 
 authStudents = Blueprint('authStudent', __name__)
 
-@authStudents.route('/students/home')
+@authStudents.route('/students/home', methods=['POST'])
 def student():
+    student_log_mail = request.form['student_log_mail']
+    student_log_password = request.form['student_log_password']
+
+    try:
+        studentLogin = Student.query.filter_by(email=teacher_log_mail).first()
+    except Exception as e:
+        print(f"Error, query student's mail {str(e)}")
+    
+    if studentLogin:
+        if pbkdf2_sha256(student_log_password, studentLogin.student_log_password):
+            pass
+        else:
+            pass
+    else:
+        flash("This email doesn't exist!")
+        return render_template('students/home.html')
+
+
     return render_template('students/home.html')
+
